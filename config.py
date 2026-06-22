f3c24d7981e45fe83f9d06c340dffa6de6c0a963
Sanika Bhalekar <65656262+sanika233@users.noreply.github.com>
2026-06-22 16:13:11 +0530

    Remove sensitive user credentials from config.py
    Removed sensitive credentials from config.py.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Credentials
 
-USERNAME = "mahagaonkarsanika@gmail.com"
-PASSWORD = "sanika@123"
+USERNAME = "---------------------"
+PASSWORD = "------------------"
 
 BASE_URL = "https://v2.zenclass.in/login"
